b3214c840097560fe659730f48a595806e2c455c
XVManBYD <marcin.grzy2006@gmail.com>
2024-08-29 12:15:47 +0200

    Added an animation for changing day of the week

--- Static/day_selector.js
+++ Static/day_selector.js
@@ -3,23 +3,26 @@ const weekdays = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek"];
 
 let days = document.querySelectorAll(".day");
 
-days[weekday].classList.toggle("shown");
-weekday_span.innerText = weekdays[weekday];
+// days[weekday].classList.toggle("shown");
+const r = document.querySelector(':root');
 
+weekday_span.innerText = weekdays[weekday];
 function prev_day() {
-    days[weekday].classList.toggle("shown");
+    // days[weekday].classList.toggle("shown");
     if (weekday > 0) {
         weekday -= 1;
     }
-    days[weekday].classList.toggle("shown");
+    // days[weekday].classList.toggle("shown");
+    r.style.setProperty("--day-move", `-${weekday * 100}vw`)
     weekday_span.innerText = weekdays[weekday];
 }
 
 function next_day() {
-    days[weekday].classList.toggle("shown");
+    // days[weekday].classList.toggle("shown");
     if (weekday < 4) {
         weekday += 1;
     }
-    days[weekday].classList.toggle("shown");
+    r.style.setProperty("--day-move", `-${weekday * 100}vw`)
+    // days[weekday].classList.toggle("shown");
     weekday_span.innerText = weekdays[weekday];
 }
